c3ee3d358328523aa7f424ad01537c3b7f59e9fb
GreaperG <s46367207@gmail.com>
2025-12-04 16:45:11 +0500

    RegistrationFormType.php

--- app/src/Form/RegistrationFormType.php
+++ app/src/Form/RegistrationFormType.php
@@ -56,6 +56,7 @@ class RegistrationFormType extends AbstractType
     {
         $resolver->setDefaults([
             'data_class' => User::class,
+            'csrf_protection => false,                  
         ]);
     }
 }
